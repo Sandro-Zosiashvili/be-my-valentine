9a7faf1237058f371a02fe71dab950965a898733
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2026-02-10 02:31:00 +0400

    fix: gif delay

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+        <head>
+            <link rel="preload" as="image" href="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExOXA1MWtvc2p5MDE0NHAyaHdvYnQxZHRraDUwbWxoZzl2MmtjczVpbSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/SqmkZ5IdwzTP2/giphy.gif" />
+        </head>
         <body>
         {children}
         </body>
